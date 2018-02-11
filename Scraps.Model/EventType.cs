56e575d5f22fdca913608f23a1ddab4e31ba3401
clutchgomez <clutchgomez@gmail.com>
2018-02-11 18:20:53 +0100

    Adding images and events, link images to events, list events

--- Scraps.Model/EventType.cs
+++ Scraps.Model/EventType.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Scraps.Model
 {
-    public partial class EventType : IEquatable<EventType>
-	{
+    public partial class EventType
+    {
         public EventType()
         {
             EventTyped = new HashSet<EventTyped>();
@@ -15,21 +14,5 @@ namespace Scraps.Model
         public string Name { get; set; }
 
         public virtual ICollection<EventTyped> EventTyped { get; set; }
-
-		public override bool Equals(object obj)
-		{
-			return Equals(obj as EventType);
-		}
-
-		public bool Equals(EventType other)
-		{
-			return other != null &&
-				   Name == other.Name;
-		}
-
-		public override int GetHashCode()
-		{
-			return 539060726 + EqualityComparer<string>.Default.GetHashCode(Name);
-		}
-	}
+    }
 }
